79773b8f87ab4f08fdb6b4692e55a63cc0c4872d
iori-28 <skyzyrym17@gmail.com>
2025-11-08 18:05:57 +0700

    recode: uts

--- Llingkaran.php
+++ Llingkaran.php
@@ -2,65 +2,36 @@
 
 class LuasLingkaran
 {
+
     public const phi = 3.14;
     public int $jari;
 
-    public function __construct($isijari = 1)
+    public function __construct($isiJari = 1)
     {
-        $this->jari = $isijari;
+        $this->jari = $isiJari;
     }
 
-    public function tampilLl($nama = 'ban')
+    public function tampil($nama = 'ban')
     {
         $rumus = LuasLingkaran::phi * ($this->jari * $this->jari);
-        echo "Lingkaran " . ($nama) . " Ini Hasil luasnya adalah: " . $rumus . "<br>";
+        echo "Lingkaran {$nama} hasilnya adalah: {$rumus}";
     }
 
     public static function testing()
     {
-        echo "</br>";
-        echo "Ini testing static<br>";
+        echo "<br/>";
+        echo "ini dari static";
     }
 
     public function __destruct()
     {
-        echo "Udah capek<br>";
+        echo "<br/>";
+        echo "udah ah cape";
     }
 }
 
-class KelilingLingkaran
-{
-    public const phi = 3.14;
-    public int $jari;
-    public function __construct($isijari = 1)
-    {
-        $this->jari = $isijari;
-    }
+$lingkaran = new LuasLingkaran(12);
+// $lingkaran->jari = 12;
+$lingkaran->tampil('roda'); //panggil method tampil
 
-    public function tampilKl($nama = 'ban')
-    {
-        $rumus2 = KelilingLingkaran::phi * (2 * $this->jari);
-        echo "Lingkaran " . ($nama) . " Hasil kelilingnya adalah: " . $rumus2 . "<br>";
-    }
-
-    public static function testing()
-    {
-        echo "</br>";
-        echo "Ini testing static keliling<br>";
-    }
-
-    public function __destruct()
-    {
-        echo "</br>";
-        echo "Udah capek keliling<br>";
-    }
-}
-
-$Lingkaran = new LuasLingkaran(10);
-$Lingkaran->tampilLl('ban');
-
-$Lingkaran2 = new KelilingLingkaran(10);
-$Lingkaran2->tampilKl('ban');
-
-LuasLingkaran::testing();
-KelilingLingkaran::testing();
+LuasLingkaran::testing(); //panggil static method
